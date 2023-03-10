66cb678ccf831dc8dad876a43092873b630b1e00
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-10 22:23:33 +0100

    1 task ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createCardsGallery(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+const cardsGallery = createCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
 
 console.log(galleryItems);
